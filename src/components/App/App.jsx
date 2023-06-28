4d370600d859ee7aabd6ed5a1589213326b5c6c7
AndriiRod <rod.andriiw@gmail.com>
2023-06-28 18:26:15 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,34 +8,34 @@ const App = () => {
   const [goodReview, setGoodReview] = useState(0);
   const [neutralReview, setNeutralReview] = useState(0);
   const [badReview, setBadReview] = useState(0);
+
   const wholeState = [goodReview, neutralReview, badReview];
+  const total = countTotalFeedback();
+  const percentPositiveRev = countPositiveFeedbackPercentage();
 
   const changingStateIncrement = name => {
     switch (name) {
       case 'good':
-        setGoodReview(prevGood => prevGood + 1);
+        setGoodReview(prevGoodReview => prevGoodReview + 1);
         break;
       case 'neutral':
-        setNeutralReview(prevNeutral => prevNeutral + 1);
+        setNeutralReview(prevNeutralReview => prevNeutralReview + 1);
         break;
       case 'bad':
-        setBadReview(prevBad => prevBad + 1);
+        setBadReview(prevBadReview => prevBadReview + 1);
         break;
       default:
         break;
     }
   };
 
-  const countTotalFeedback = () => {
+  function countTotalFeedback() {
     return wholeState.reduce((acc, currentValue) => acc + currentValue, 0);
-  };
+  }
 
-  const countPositiveFeedbackPercentage = () => {
+  function countPositiveFeedbackPercentage() {
     return total === 0 ? 0 : Math.round((goodReview / total) * 100);
-  };
-
-  const total = countTotalFeedback();
-  const percentPositiveRev = countPositiveFeedbackPercentage();
+  }
 
   return (
     <Section title="Please leave feedback">
